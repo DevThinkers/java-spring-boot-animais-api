2a0345d26a6f8c311abb6a419bec7dd3bf920372
Joao Emilio <joaoemilio@devthinkers.com>
2017-09-18 20:27:38 -0300

    atualizando pom.xml para gerar um war file

--- start-mysql.sh
+++ start-mysql.sh
@@ -1,1 +1,4 @@
+echo "Iniciar container docker mysql-animais"
 docker run -d -p 3306:3306 mysql-animais
+echor "Verificar se o container subiu corretamente"
+docker ps
--- stop-mysql.sh
+++ stop-mysql.sh
@@ -1,3 +1,5 @@
 DOCKER_INSTANCE=`docker ps | grep mysql-animais | awk '{print $NF}'`
-echo $DOCKER_INSTANCE
+echo "Nome do container a ser interrompido: " $DOCKER_INSTANCE
 docker stop $DOCKER_INSTANCE
+echo "Verificar se o container parou"
+docker ps
